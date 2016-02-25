97f119a2bf65881b172341b41309755f4894ff43
Barak Korren <bkorren@redhat.com>
2016-02-25 23:57:30 +0200

    Solved the excercize

--- string_ext.rb
+++ string_ext.rb
@@ -1,2 +1,14 @@
 class StringExt
+  def initialize(first, second)
+    @first = first
+    @second = second
+  end
+
+  def concat
+    @first + @second
+  end
+
+  def substract
+    @first.sub(@second, '')
+  end
 end
